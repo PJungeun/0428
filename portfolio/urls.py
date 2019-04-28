2cc716bd9ad0f70321748168c86a334301eb9aba
PJungeun <719wjddms@naver.com>
2019-04-28 23:36:07 +0900

    first commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,9 +4,9 @@ import portfolio.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/home/', portfolio.views.home, name='home'),
-    path('portfolio/home/person/<int:person_id>/', portfolio.views.detail, name='detail'),
-    path('portfolio/person/new/', portfolio.views.person_new, name='new'),
-    path('portfolio/<int:person_id>/edit/', portfolio.views.person_edit, name='edit'),
-    path('portfolio/<int:person_id>/delete/', portfolio.views.person_delete, name='delete'),
+    path('home/', portfolio.views.home, name='home'),
+    path('person/<int:person_id>/', portfolio.views.detail, name='detail'),
+    path('person/new/', portfolio.views.person_new, name='new'),
+    path('<int:person_id>/edit/', portfolio.views.person_edit, name='edit'),
+    path('<int:person_id>/delete/', portfolio.views.person_delete, name='delete'),
 ]
